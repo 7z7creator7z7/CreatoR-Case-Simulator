6fd34f988213d3ad14cfade63375d85ef3940391
7z7creator7z7 <7z7creator7z7@gmail.com>
2026-05-17 12:02:23 +0500

    script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const caseData = [
     { name: "📦 Oddiy", price: 1, skins: allSkins.slice(0, 5) },
     { name: "🔰 Elite", price: 10, skins: allSkins.slice(0, 5) },
     { name: "🎰 Lucky", price: 100, skins: allSkins.slice(0, 5) }, 
-    { name: "🎰 Best Lucky", price: 1000000, skins: allSkins.slice(0, 5) }
+    { name: "🎰 Best Lucky", price: 280116, skins: allSkins.slice(0, 5) }
 ];
 
 function updateLanguageUI() {
